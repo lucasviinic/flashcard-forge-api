6dd6a1b83a49208ce6e842f06e7375e897ad5616
Lucas Vinicius <lvms@cin.ufpe.br>
2025-02-16 21:53:12 -0300

    feat: implement upload subject image to firebase cloud storage

--- routers/subjects.py
+++ routers/subjects.py
@@ -1,12 +1,20 @@
+from datetime import datetime, timezone
+import os
 from typing import Annotated
 from starlette import status
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, File, HTTPException, Query, UploadFile
 
 from models.requests_model import SubjectRequest
+from models.subject_model import Subjects
 from usecases.auth import get_current_user_usecase
 from database import db_dependency
 from usecases.subjects import create_subject_usecase, delete_subject_usecase, retrieve_all_subjects_usecase, retrieve_subject_usecase, update_subject_usecase
+
+import firebase_admin
+from firebase_admin import credentials, storage
+
+from google.cloud.exceptions import NotFound
 
 
 router = APIRouter(
@@ -15,6 +23,12 @@ router = APIRouter(
 )
 
 user_dependency = Annotated[dict, Depends(get_current_user_usecase)]
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate("flashcardforge-firebase-adminsdk.json")
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': f"{os.getenv('FIREBASE_PROJECT_ID')}.firebasestorage.app"
+    })
 
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def create_subject(db: db_dependency, user: user_dependency, subject_request: SubjectRequest):    
@@ -64,3 +78,44 @@ async def delete_subject(user: user_dependency, db: db_dependency, subject_id: s
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='authentication failed')
     
     delete_subject_usecase(db, subject_id, user_id=user.get('id'))
+
+@router.put("/{subject_id}/upload-image", status_code=status.HTTP_200_OK)
+async def update_subject_image(
+    subject_id: str,
+    user: user_dependency,
+    db: db_dependency,
+    file: UploadFile = File(...)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='authentication failed')
+
+    bucket = storage.bucket()
+    blob = bucket.blob(f"subject-images/{subject_id}")
+
+    try: blob.delete()
+    except NotFound: pass
+
+    try:
+        blob.upload_from_file(file.file, content_type=file.content_type)
+        blob.make_public()
+        image_url = f"{blob.public_url}?v={datetime.now(timezone.utc).time()}"
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao fazer upload da imagem para o Firebase: {str(e)}")
+
+    subject_model = db.query(Subjects).filter(
+        Subjects.id == subject_id,
+        Subjects.user_id == user.get('id'),
+        Subjects.deleted_at == None
+    ).first()
+
+    if not subject_model:
+        raise HTTPException(status_code=404, detail='subject not found')
+    
+    subject_model.image_url = image_url
+    subject_model.updated_at = datetime.now(timezone.utc)
+
+    db.add(subject_model)
+    db.commit()
+    db.refresh(subject_model)
+
+    return subject_model.to_dict()
