e7bc81977ba07141583a54158aee9303e077b797
Lucas Vinicius <lvms@cin.ufpe.br>
2025-02-24 21:18:12 -0300

    feat: improve ai prompt

--- core/openai/client.py
+++ core/openai/client.py
@@ -9,6 +9,7 @@ from utils import constants
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 def flash_card_generator(prompt: str, history: list, quantity: int, difficulty: int = 1):
+    model = os.getenv('DEFAULT_MODEL')
     max_attempt = 3
     attempt = 0
 
@@ -16,13 +17,15 @@ def flash_card_generator(prompt: str, history: list, quantity: int, difficulty: 
 
     while True:
         try:
-            model = os.getenv('DEFAULT_MODEL')
             system_prompt = f"""
             ## Você é um gerador de flashcards e apenas um gerador de flashcards. Com base em todo texto que receber, você deve apenas gerar {quantity} ótimos flashcards de grau {difficulty_levels[difficulty]} sobre o conteúdo do texto, não faça perguntas triviais, apenas perguntas de nível abordado em provas. Você deve gerar apenas perguntas e respostas contendo o que pode ser encontrado no texto fornecido. Você é um gerador de flashcards e deve gerar apenas flashcards.
-            ## Você SEMPRe deve gerar {quantity} fleshcards. Gere exatamente {quantity} flashcards. Não gere nem mais nem menos do que {quantity}, gere exatamente {quantity} flashcards.
-            ## Você deve entregar o resultado no formato JSON. Você SEMPRE deve retornar o resultado no formato JSON, contendo uma lista de flashcards no seguinte formato:
+            ## SEMPRE que houver exemplos no texto, ou algo do tipo, não responda sobre o exemplo e sim sobre o assunto, SEMPRE sobre o assunto abordado no texto.
+            ## Você SEMPRE deve criar os flashcards com perguntas e respostas bem elaboradas (nada simples e bobo).
+            ## Você SEMPRE deve gerar {quantity} fleshcards. Gere exatamente {quantity} flashcards. Não gere nem mais nem menos do que {quantity}, gere exatamente {quantity} flashcards.
+            ## Certifique-se de ter criado exatamente {quantity} flashcards. Deve ter exatamente {quantity} flashcards.
+            ## Você SEMPRE deve entregar o resultado no formato JSON. Você SEMPRE deve retornar o resultado no formato JSON, contendo uma lista de flashcards no seguinte formato:
             {json.dumps(constants.FLASHCARDS_RESPONSE_TEMPLATE)}
-            ## Histórico de flashcards gerados (caso haja conteúdo abaixo, não repita)
+            ## Abaixo, está o histórico de flashcards gerados (caso haja conteúdo abaixo, não repita)
             {str(history)}
             """
             response = client.chat.completions.create(
@@ -55,6 +58,5 @@ def flash_card_generator(prompt: str, history: list, quantity: int, difficulty: 
         except Exception as error:
             attempt += 1
             if attempt >= max_attempt:
-                return "Error on GPT3: %s" % error
-            print('Error on comunicate with OpenAI:', error)
+                return f"Error on model {model}: %s" % error
             sleep(1)
